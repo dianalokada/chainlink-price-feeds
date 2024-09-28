78f7410c05ae681e29612c86d7302a002a0ef175
Diana <dianaalokhina@gmail.com>
2024-09-28 14:49:19 -0400

    create boilerplate and 2 contractes templates

---
+++ contracts/FundMe.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+// 1. Pragma
+pragma solidity ^0.8.7;
+// 2. Imports
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./PriceConverter.sol";
+
+// 3. Interfaces, Libraries, Contracts
+error FundMe__NotOwner();
+
+/**@title A sample Funding Contract
+ * @author Patrick Collins
+ * @notice This contract is for creating a sample funding contract
+ * @dev This implements price feeds as our library
+ */
+contract FundMe {
+    
+}
