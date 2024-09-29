95c75b5c4e76c09830732f3cafe05c58ec2ec364
Diana <dianaalokhina@gmail.com>
2024-09-29 17:59:27 -0400

    create getPrice, convert, fund and withdraw functions

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -5,14 +5,42 @@ pragma solidity ^0.8.7;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "./PriceConverter.sol";
 
-// 3. Interfaces, Libraries, Contracts
-error FundMe__NotOwner();
+error FundMe_NotOwner();
 
-/**@title A sample Funding Contract
- * @author Patrick Collins
- * @notice This contract is for creating a sample funding contract
- * @dev This implements price feeds as our library
- */
 contract FundMe {
-    
+    //
+    using PriceConverter for uint256;
+
+    uint public constant MINIMUM_USD = 50 * 1 ether;
+    address public immutable i_owner;
+    address[] public s_funders;
+    mapping(address => uint256) private s_addressToAmountFunded;
+    AggregatorV3Interface private s_priceFeed;
+
+    modifier onlyoWNER {
+        if (msg.sender != i_owner) revert FundMe_NotOwner();
+        _;
+    }
+
+    constructor(address priceFeed) {
+        i_owner = msg.sender;   
+        s_priceFeed = AggregatorV3Interface(priceFeed);
+    }
+
+//a function to send money to a contract
+    function fund() public payable {
+        require(msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD, "You need to spend more ETH!");
+        s_funders.push(msg.sender);
+        s_addressToAmountFunded[msg.sender] += msg.value;
+    }
+
+    function withdraw() public onlyoWNER {
+        for (uint256 funderIndex = 0; funderIndex < s_funders.length; funderIndex++) {
+            address funder = s_funders[funderIndex];
+            s_addressToAmountFunded[funder] = 0;
+        }
+        s_funders = new address[](0);
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+    }
 }
--- contracts/PriceConverter.sol
+++ contracts/PriceConverter.sol
@@ -4,5 +4,21 @@ pragma solidity ^0.8.7;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 library PriceConverter {
-  
+    //a fucntion to get a price for ethereum in US dollars
+  function getPrice(AggregatorV3Interface priceFeed) internal view returns (uint256) {
+    //it returns 5 things so thats is why 5 comas - bc we just need 1 thing
+    (, int256 answer, , , ) = priceFeed.latestRoundData();
+    //the answer here gives us the answer with 8 decimals but we need to have 10 decimals.
+    //1 ether = 10 ** 18 
+    //so we need to multiply the answer by 10 decimal places
+    return uint256(answer * 10000000000);
+  } 
+
+  function getConversionRate(uint256 ethAmount, AggregatorV3Interface priceFeed) internal view returns (uint256) {
+    //get the price of eth in usd
+    uint256 ethPrice = getPrice(priceFeed);
+    //get the price and multiply by the amount of ethereum. the amount is gonna be in wei
+    uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1 ether;
+    return ethAmountInUsd;
+  }
 }
